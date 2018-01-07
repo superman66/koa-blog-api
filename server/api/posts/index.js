f3914abe1812ab6012017447cb5de03c849f19b1
Superman <supermanchc@gmail.com>
2018-01-08 00:23:04 +0800

    Update post api

--- server/api/posts/index.js
+++ server/api/posts/index.js
@@ -1,16 +1,17 @@
 import Router from 'koa-router'
 import PostController from './post.controller'
 
-const adminPrefix = '/admin/post'
+const adminPrefix = '/admin/posts'
 const router = new Router()
 // admin api
-router.get(`${adminPrefix}/posts`, PostController.posts)
-router.get(`${adminPrefix}/posts/:id`, PostController.detail)
-router.post(`${adminPrefix}/posts`, PostController.add)
-router.patch(`${adminPrefix}/posts/:id`, PostController.update)
-router.post(`${adminPrefix}/posts/changeStatus/:id`, PostController.changeStatus)
-router.del(`${adminPrefix}/posts/:id`, PostController.remove)
+router.get(`${adminPrefix}`, PostController.posts)
+router.get(`${adminPrefix}/:id`, PostController.detail)
+router.post(`${adminPrefix}`, PostController.add)
+router.patch(`${adminPrefix}/:id`, PostController.update)
+router.post(`${adminPrefix}/changeStatus/:id`, PostController.changeStatus)
+router.del(`${adminPrefix}/:id`, PostController.remove)
 
 // front api
 router.get('posts', PostController.posts)
+router.get('posts/:id', PostController.detail)
 export default router
